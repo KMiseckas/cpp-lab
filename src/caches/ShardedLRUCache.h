6861464cbfdc310f595b15b356f4fc991112175c
Klaudijus <klaudmise@gmail.com>
2026-08-19 21:29:17 +0100

    Added comments.

--- src/caches/ShardedLRUCache.h
+++ src/caches/ShardedLRUCache.h
@@ -6,6 +6,14 @@
 #include <mutex>
 #include <utility>
 
+/**
+ * Sharder and thread safe LRU Cache
+ * - Sharding, LRU cache consists of shards, each one basically a small LRU cache. Helps higher concurrency access as multiple shards can be
+ * accessed at the same time.
+ * - Downside, no global least used values. LRU per shard only.
+ * - Current implementation has no capacity adjustment. A single shard may experience high usage but not do anything about it to relieve pressure on it.
+ * - No exception handling (in this implementation). Could try catch several areas to safely exit functions without invalidating cache.
+ */
 template<typename Key, typename Val, std::size_t Capacity, std::size_t ShardCount = 8U>
 class ShardedLRUCache
 {
@@ -18,21 +26,21 @@ class ShardedLRUCache
         using KVListIter = std::list<std::pair<Key, Val>>::iterator;
 
         mutable std::mutex mutex;
-        std::list<std::pair<Key, Val>> valueOrder;
-        std::unordered_map<Key, KVListIter> keyValueMap;
+        std::list<std::pair<Key, Val>> valueOrder; //Double linked list of KV Pair nodes, holds the actual value. Allows quick reordering - O(1)
+        std::unordered_map<Key, KVListIter> keyValueMap; // Map, holds iterator of list for quick access based on key - O(1)
         std::size_t capacity{0U};
     };
 
 public:
     ShardedLRUCache()
     {
-        std::size_t basePerShard{Capacity / ShardCount};
-        std::size_t remainder{Capacity % ShardCount};
+        std::size_t basePerShard{Capacity / ShardCount}; //Get base capacity to go into each shard.
+        std::size_t remainder{Capacity % ShardCount}; // Remainder of what capscity left to assign after bases go into shards.
 
         for(std::size_t i{0}; i < ShardCount; ++i)
         {
             auto& shard = m_shards[i];
-            shard.capacity = basePerShard + (i < remainder ? 1 : 0);
+            shard.capacity = basePerShard + (i < remainder ? 1 : 0); // Add 1 additional capacity per shard until past remainder. Meets full capacity criteria.
             shard.keyValueMap.reserve(shard.capacity);
         }
     }
@@ -87,6 +95,9 @@ public:
         return true;
     }
 
+    /**
+     * Just a approx snapshot, not guaranteed to be exact if a shard thats not locked has a value evicted/added.
+     */
     std::size_t getSize() const
     {
         std::size_t size{0U};
